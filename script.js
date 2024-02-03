f1c28db837e574926bb4e01088c88a36be98906e
neelam <null.0990.null@gmail.com>
2024-02-03 19:49:59 +0530

    addToCartMsg issue fixed

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-console.log("Connected");
+// console.log("Connected");
+
+function calculatePercentageOff(price, comparePrice) {
+  const percentageOff = ((comparePrice - price) / comparePrice) * 100;
+  return parseInt(percentageOff) + "%";
+}
 
 function populateData(data) {
   const product = data.product;
@@ -9,11 +14,15 @@ function populateData(data) {
   const title = document.querySelector(".product-title");
   title.innerText = product.title;
 
-  // const actual_price = document.querySelector(".actual-price");
-  // actual_price.innerText = product.price;
-
   const compare_price = document.querySelector(".compare-price");
   compare_price.innerText = product.compare_at_price;
+
+  const price = parseInt(product.price.slice(1));
+  const c_price = parseInt(product.compare_at_price.slice(1));
+  const discount = calculatePercentageOff(price, c_price);
+
+  const actual_price = document.querySelector(".actual-price");
+  actual_price.innerHTML = `${product.price}  <span class="discount">${discount} Off</span>`;
 
   const description = document.querySelector(".description");
   description.innerHTML = product.description;
@@ -21,13 +30,30 @@ function populateData(data) {
   const square_radio = document.querySelectorAll(".colors .checkmark");
   for (let i = 0; i < product.options[0].values.length; i++) {
     square_radio[i].style.setProperty(
-      "--color",
+      `--color`,
       Object.values(product.options[0].values[i])[0]
+    );
+
+    // console.log(Object.keys(product.options[0].values[i])[0]);
+
+    // console.log(square_radio[i]);
+  }
+
+  const allSize = document.querySelectorAll("input[name='size']");
+  for (let i = 0; i < product.options[1].values.length; i++) {
+    allSize[i].setAttribute(`id`, product.options[1].values[i]);
+  }
+  // console.log("allsize", allSize);
+
+  const allColors = document.querySelectorAll("input[name='color']");
+  for (let i = 0; i < product.options[0].values.length; i++) {
+    allColors[i].setAttribute(
+      `id`,
+      Object.keys(product.options[0].values[i])[0]
     );
   }
 
   const size = document.querySelectorAll(" .size label");
-  console.log(size);
   for (let i = 0; i < product.options[1].values.length; i++) {
     size[i].innerText = product.options[1].values[i];
   }
@@ -39,19 +65,36 @@ fetch(
 )
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
-
     populateData(data);
   });
 
-function calculatePercentageOff(price, comparePrice) {
-  const percentageOff = ((comparePrice - price) / comparePrice) * 100;
-  return percentageOff.toFixed(2) + "%";
-}
+const allColors = document.querySelectorAll("input[name='color']");
+
+let selected_color = "Yellow";
+allColors.forEach((radio) => {
+  radio.addEventListener("change", () => {
+    if (radio.checked) {
+      selected_color = radio.getAttribute("id");
+    }
+  });
+});
+
+const allSize = document.querySelectorAll("input[name='size']");
+let selected_size = "Small";
+allSize.forEach((radio) => {
+  radio.addEventListener("change", () => {
+    if (radio.checked) {
+      selected_size = radio.getAttribute("id");
+    }
+  });
+});
 
 function addToCart() {
+  const title = document.querySelector(".product-title").innerText;
+
   const x = document.getElementById("addToCartMessage");
   x.style.display = "inline-flex";
+  x.innerText = `${title} with Color ${selected_color} and Size ${selected_size} added to cart`;
 }
 
 // update quantity
@@ -79,7 +122,6 @@ inc.addEventListener("click", () => {
 
 dec.addEventListener("click", () => {
   const inputValue = parseInt(input.value, 10);
-  console.log(inputValue);
 
   if (inputValue > 1) {
     decrement();
